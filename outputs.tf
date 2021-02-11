2f49acb3e4f90c71c0b311b6c6b9964a47e97406
Fionn Masuhr <fmasuhr@babbel.com>
2021-02-11 12:44:13 +0100

    Add security group output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "aws_security_group" {
+  description = "The Security Group used by the ALB"
+  value       = aws_security_group.this
+}
+
 output "this" {
   description = "ALB"
   value       = aws_lb.this
