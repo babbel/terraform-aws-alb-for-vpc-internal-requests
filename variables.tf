8ad576c11eb9925d43e253c498205f5e1b43a37d
Jan Sebastian Siwy <github@jansiwy.de>
2024-09-23 11:06:09 +0200

    Unify structure of module variables

--- variables.tf
+++ variables.tf
@@ -1,52 +1,66 @@
 variable "drop_invalid_header_fields" {
-  description = "Specify if the ALB should drop invalid header fields"
-
   type    = bool
   default = true
+
+  description = <<EOS
+Specify if the ALB should drop invalid header fields.
+EOS
 }
 
 variable "ingress_port" {
-  description = "The port the ALB will listen to"
-
   type    = number
   default = 80
+
+  description = <<EOS
+The port the ALB will listen to.
+EOS
 }
 
 variable "name" {
-  description = "Name of the ALB"
-
   type = string
+
+  description = <<EOS
+Name of the ALB.
+EOS
 }
 
 variable "subnets" {
-  description = "List of subnets the ALB will be created in"
-
   type = list(
     object({
       id = string
     })
   )
+
+  description = <<EOS
+List of subnets the ALB will be created in.
+EOS
 }
 
 variable "tags" {
-  description = "Map of tags to assign to all resources supporting tags (in addition to the `Name` tag)"
-
   type    = map(string)
   default = {}
+
+  description = <<EOS
+Map of tags assigned to all AWS resources created by this module.
+EOS
 }
 
 variable "target_group" {
-  description = "Target group all requests to the ALB will be forwarded to"
-
   type = object({
     arn = string
   })
+
+  description = <<EOS
+Target group all requests to the ALB will be forwarded to.
+EOS
 }
 
 variable "vpc" {
-  description = "VPC the ALB and the security group will be created in"
-
   type = object({
     id = string
   })
+
+  description = <<EOS
+VPC the ALB and the security group will be created in.
+EOS
 }
